a497c0b67ebedc3a272475da76cfbe57ea82b1e7
Andrey <147320074+ilupat@users.noreply.github.com>
2023-12-12 16:42:09 +0300

    Added changes

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,10 +5,13 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
+
         HelloWorld bean1 = (HelloWorld) applicationContext.getBean("helloworld");
         HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
+
         Cat cat1 = applicationContext.getBean("cat",Cat.class);
         Cat cat2 = applicationContext.getBean("cat",Cat.class);
+
         System.out.println(bean1==bean2);
         System.out.println(cat1==cat2);
 
